e0fb9f0d7d88e3e0e268619bd209053f1632b081
Mahmood Ahmad <mahmood.ahmad@constellationdealer.com>
2025-01-01 12:07:18 +0500

    added comment

--- cypress/e2e/3 - Sample/tests.cy.js
+++ cypress/e2e/3 - Sample/tests.cy.js
@@ -1,3 +1,5 @@
+// first test
+
 describe("My First Test", () => {
   it("Visits the Kitchen Sink", () => {
     cy.visit("https://example.cypress.io");
@@ -8,3 +10,5 @@ describe("My First Test", () => {
       .should("have.value", "fake@email.com");
   });
 });
+
+
